3e9b12ad3e4828b04d22a263062882c8fe42839c
Justin Choi <justin7623@gmail.com>
2024-05-05 01:04:24 +0800

    Finished Hello World

--- hello/hello.go
+++ hello/hello.go
@@ -3,6 +3,23 @@ package main
 func main() {
 }
 
-func SayHello(name string) string {
-	return "Hello, " + name
+const (
+	englishGreeting = "Hello, "
+	spanishGreeting = "Hola, "
+	frenchGreeting  = "Bonjour, "
+)
+
+func SayHello(name string, lang string) string {
+
+	if name == "" {
+		name = "world"
+	}
+
+	switch lang {
+	case "Spanish":
+		return spanishGreeting + name
+	case "French":
+		return frenchGreeting + name
+	}
+	return englishGreeting + name
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,32 @@ package main
 import "testing"
 
 func TestSayHello(t *testing.T) {
-	actual := SayHello("Justin")
-	expected := "Hello, Justin"
+	t.Run("say hello to a person", func(t *testing.T) {
+		actual := SayHello("Justin", "")
+		expected := "Hello, Justin"
+		assertEquals(t, actual, expected)
+	})
 
+	t.Run("say hello to the world when the person is absent", func(t *testing.T) {
+		actual := SayHello("", "")
+		expected := "Hello, world"
+		assertEquals(t, actual, expected)
+	})
+
+	t.Run("say hello in Spanish", func(t *testing.T) {
+		actual := SayHello("Carlos", "Spanish")
+		expected := "Hola, Carlos"
+		assertEquals(t, actual, expected)
+	})
+
+	t.Run("say hello in French", func(t *testing.T) {
+		actual := SayHello("Alex", "French")
+		expected := "Bonjour, Alex"
+		assertEquals(t, actual, expected)
+	})
+}
+
+func assertEquals(t *testing.T, actual string, expected string) {
 	if actual != expected {
 		t.Errorf("actual = %q, expected = %q", actual, expected)
 	}
